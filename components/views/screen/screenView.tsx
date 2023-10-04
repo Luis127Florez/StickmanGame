38406e773575d776072cc624afe179a1118aeb73
luisf <junior@wowdesarrolos.com>
2023-10-04 18:33:12 -0500

    redux 1

--- components/views/screen/screenView.tsx
+++ components/views/screen/screenView.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import ButtonPlayImg from "../../../public/img/boton-de-play.png";
 import { incrementBy } from "../../../redux/avatar/avatarExtraReducer";
--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+    emotion: true,
+  },
+  output: 'standalone',
   experimental: {
     typedRoutes: true,
   },
