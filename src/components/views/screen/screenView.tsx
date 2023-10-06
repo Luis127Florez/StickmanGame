7473767b059acce8000b1724015951389cb4bd6a
luisf <junior@wowdesarrolos.com>
2023-10-06 18:39:42 -0500

    estado no atrapado

--- src/components/views/screen/screenView.tsx
+++ src/components/views/screen/screenView.tsx
@@ -1,14 +1,58 @@
 "use client";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+import React, { useEffect, useState } from "react";
 import ButtonPlayImg from "../../../../public/img/boton-de-play.png";
-import { incrementBy } from "../../../redux/avatar/avatarExtraReducer";
-import { useDispatch } from "react-redux";
+import avatarUp from "../../../../public/img/arriba.png";
+import avatarBack from "../../../../public/img/atras.png";
+import avatarForward from "../../../../public/img/adelante.png";
+import avatarDown from "../../../../public/img/abajo.png";
+import { moveForward } from "../../../redux/avatar/avatarExtraReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { avatarState } from "../../../models/avatar/avatarState";
 
 export const ScreenView = () => {
   const dispatch = useDispatch();
+
+  const avatarProperties = useSelector(avatarState);
+  console.log(avatarProperties);
+
+  const [avatar, setAvatar] = useState<StaticImageData>();
+
+  useEffect(() => {
+    document.addEventListener(
+      "keydown",
+      (event) => {
+        const keyValue = event.key;
+        const codeValue = event.code;
+        switch (codeValue) {
+          case "ArrowUp":
+            setAvatar(avatarUp);
+            break;
+          case "ArrowRight":
+            setAvatar(avatarForward);
+            break;
+          case "ArrowLeft":
+            setAvatar(avatarBack);
+            break;
+          case "ArrowDown":
+            setAvatar(avatarDown);
+            break;
+
+          default:
+            break;
+        }
+      },
+      false
+    );
+  }, []);
+
   return (
     <div className="container">
-      <div id="screen" className="screen"></div>
+      <div id="screen" className="screen">
+        {avatar && (
+          <Image id="playButton" width={59} height={59} src={avatar} alt="" />
+        )}
+      </div>
       <div className="control">
         <Image
           id="playButton"
@@ -21,7 +65,7 @@ export const ScreenView = () => {
       <button
         onClick={() => {
           console.log("asd");
-          dispatch(incrementBy());
+          dispatch(moveForward(2));
         }}
       >
         dispara
--- src/redux/avatar/avatarExtraReducer.ts
+++ src/redux/avatar/avatarExtraReducer.ts
@@ -2,12 +2,14 @@ import { createAction } from "@reduxjs/toolkit";
 import { TExtraReducer } from "./types";
 import { avatarState } from "../../models/avatar/avatarState";
 
-export const incrementBy = createAction('incrementBy')
+export const moveForward = createAction<number>("moveForward");
 
-
-export const avatarExtraReducer: TExtraReducer< typeof avatarState> = (builder)  => {
-    builder.addCase (incrementBy, (state, action) => {
-        console.log(state, action , '௹௹');
-        return state
-    })
+export const avatarExtraReducer: TExtraReducer<typeof avatarState> = (
+  builder
+) => {
+  builder.addCase(moveForward, (state, action) => {
+    state.right += action?.payload
+    console.log(state, action, "௹௹");
+    return state;
+  });
 };
