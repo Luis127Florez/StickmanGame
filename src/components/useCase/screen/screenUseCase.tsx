610e6f4063bc786aeae30d78d3fa8f8588461fa8
Luis_Florez <lflorez@wowdesarrollos.com>
2024-02-02 14:05:22 -0500

    clase

--- src/components/useCase/screen/screenUseCase.tsx
+++ src/components/useCase/screen/screenUseCase.tsx
@@ -14,38 +14,12 @@ import {
 } from "../../../redux/avatar/avatarExtraReducer";
 import { checkDevouring } from "../../../controllers/avatar/avatarFunctions";
 import { TFCheckDevouring } from "../../types/screenTypes/screenTypes";
-import * as THREE from "three";
-import { GLTFLoader } from "../../../../js/GLTFLoader";
-import { startSceneAndCamera } from "../../../controllers/avatar/avatarGraphic";
 import { useDispatch, useSelector } from "react-redux";
 
 export const ScreenUseCase = () => {
   const dispatch = useDispatch();
   const avatarProperties = useSelector(avatarState);
   const [avatar, setAvatar] = useState<StaticImageData>();
-  const [isAvatarInAir, setIsAvatarInAir] = useState<boolean>();
-  // const [model, setModel] = useState<any>();
-
-  const jump = async (jumpLevel: number = 1) => {
-    let limit = 0;
-    if (jumpLevel === 1) {
-      limit = 70;
-    }
-    if (jumpLevel === 2) {
-      limit = 100;
-    }
-    for (let index = 0; index < limit; index++) {
-      const { isDevouring, typesDevouring } = checkDevouring(document);
-      const typeDevouring = typesDevouring.find((type) => type === "top");
-      if (isDevouring && typeDevouring) return;
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(resolve);
-        }, 10);
-      });
-      dispatch(moveUp(2));
-    }
-  };
 
   useEffect(() => {
     setInterval(() => {
@@ -59,140 +33,39 @@ export const ScreenUseCase = () => {
     }, 15);
   }, []);
 
-  useEffect(() => {
-    const { scene, camera } = startSceneAndCamera();
-
-    const loader = new GLTFLoader();
-    loader.load(
-      "/robot/RobotExpressive.glb",
-      function (gltf: any) {
-        const model1 = gltf.scene;
-
-        model1.scale.set(0.3, 0.3, 0.3);
-
-        let i = 0;
-        setInterval(() => {
-          scene.add(model1);
-          // el ultimo es lejos o cerca con respecto al fondo
-          // el segundo es arriba abajo
-          model1.position.set(i, 2, -3);
-          i += 0.9;
-          model1.rotation.y = Math.PI / 2;
-
-          model1.scale.set(0.3, 0.3, 0.3);
-          setTimeout(() => {
-            scene.remove(model1);
-          }, 500);
-        }, 1000);
-
-        // setModel(model1);
-        // createGUI(model, gltf.animations);
-      },
-      undefined,
-      function (e: Error) {
-        console.error(e);
-      }
-    );
-
-    const renderer = new THREE.WebGLRenderer();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    const sceneRobot = document.getElementById("sceneRobot");
-    sceneRobot?.append(renderer.domElement);
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-
-    function asd() {
-      requestAnimationFrame(asd);
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-
-      renderer.render(scene, camera);
-    }
-    asd();
-  }, []);
-
-  /*   useEffect(() => {
-      let i = 0;
-      if (model) {
-        console.log(model);
-        setInterval(() => {
-          console.log(i);
-          model.position.set(i, i, -3);
-          i++;
-        }, 2000);
-      }
-    }, [model]); */
-
   const moveAvatarToUp = () => {
-    if (isAvatarInAir) return;
-
-    setIsAvatarInAir(true);
-
     setAvatar(avatarUp);
-
-    jump(1);
-
-    setIsAvatarInAir(false);
   };
 
-  const moveAvatarToRight = ({
-    isDevouring,
-    typesDevouring,
-  }: TFCheckDevouring) => {
+  const moveAvatarToRight = () => {
     setAvatar(avatarForward);
-    const typeDevouring = typesDevouring.find((type) => type === "right");
-    if (isDevouring && typeDevouring) {
-      return;
-    }
-    dispatch(moveForward(4));
   };
 
-  const moveAvatarToLeft = ({
-    isDevouring,
-    typesDevouring,
-  }: TFCheckDevouring) => {
+  const moveAvatarToLeft = () => {
     setAvatar(avatarBack);
-    const typeDevouring = typesDevouring.find((type) => type === "left");
-    if (isDevouring && typeDevouring) {
-      return;
-    }
-    dispatch(moveBack(4));
   };
 
-  const moveAvatarToDown = ({
-    isDevouring,
-    typesDevouring,
-  }: TFCheckDevouring) => {
+  const moveAvatarToDown = () => {
     setAvatar(avatarDown);
-    const typeDevouring = typesDevouring.find((type) => type === "bottom");
-    if (isDevouring && typeDevouring) {
-      return;
-    }
-    dispatch(moveDown(4));
   };
 
   useEffect(() => {
     document.addEventListener(
       "keydown",
       (event) => {
-        const checkedDevouring = checkDevouring(document);
         const codeValue = event.code;
-        switch (codeValue) {
-          case "ArrowUp":
-            moveAvatarToUp();
-            break;
-          case "ArrowRight":
-            moveAvatarToRight(checkedDevouring);
-            break;
-          case "ArrowLeft":
-            moveAvatarToLeft(checkedDevouring);
-            break;
-          case "ArrowDown":
-            moveAvatarToDown(checkedDevouring);
-            break;
-          default:
-            break;
+
+        if (codeValue === "ArrowUp") {
+          moveAvatarToUp();
+        }
+        if (codeValue === "ArrowRight") {
+          moveAvatarToRight();
+        }
+        if (codeValue === "ArrowLeft") {
+          moveAvatarToLeft();
+        }
+        if (codeValue === "ArrowDown") {
+          moveAvatarToDown();
         }
       },
       false
